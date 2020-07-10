e22fa5a8a6b4a873e6f1a6306e691474e43ea51f
guptaprakhariitr <prakharpguptag@gmail.com>
2020-07-11 01:49:12 +0530

    yessssss

---
+++ app/src/main/java/com/example/getthetrack/postcovid.java
@@ -0,0 +1,14 @@
+package com.example.getthetrack;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class postcovid extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.postcovid);
+    }
+}
